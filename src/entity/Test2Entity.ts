b755088d79d5ace9fca1f5cad2804c6146e9ee24
wener <wenermail@gmail.com>
2024-09-06 17:34:32 +0800

    update is-1

--- src/entity/Test2Entity.ts
+++ src/entity/Test2Entity.ts
@@ -1,12 +1,29 @@
 import { Entity, Property, types } from '@mikro-orm/core';
+import { Feature } from '@wener/nestjs';
 import {
+  EntityFeature,
   StandardBaseEntity,
-  withCodeEntity,
+  withCodeEntity as withCodeEntity2,
   withMetadataEntity,
   withNotesEntity,
   withTagsEntity,
+  type HasCodeEntity,
 } from '@wener/nestjs/entity';
-import { mixin } from '@wener/utils';
+import { mixin, type Constructor } from '@wener/utils';
+
+let withThisWillFail = withCodeEntity2;
+
+// copy from @wener/nestjs/entity to here will make the property work
+function withCodeEntity<TBase extends Constructor>(Base: TBase) {
+  @Feature([EntityFeature.HasCode])
+  @Entity({ abstract: true })
+  abstract class HasCodeMixinEntity extends Base implements HasCodeEntity {
+    @Property({ type: types.string, nullable: true })
+    code?: string;
+  }
+
+  return HasCodeMixinEntity;
+}
 
 @Entity({ tableName: 'test_1' })
 export class Test1Entity extends mixin(
@@ -21,7 +38,13 @@ export class Test1Entity extends mixin(
 }
 
 @Entity({ tableName: 'test_2' })
-export class Test2Entity extends mixin(StandardBaseEntity, withTagsEntity, withMetadataEntity, withNotesEntity) {
+export class Test2Entity extends mixin(
+  StandardBaseEntity,
+  withTagsEntity,
+  withMetadataEntity,
+  withCodeEntity,
+  withNotesEntity,
+) {
   @Property({ type: types.string, nullable: true })
   displayName?: string;
 }
--- src/runTest1.ts
+++ src/runTest1.ts
@@ -1,12 +1,19 @@
+import { Entity, MetadataStorage } from '@mikro-orm/core';
+import { checkMikroOrmEnv } from '@wener/nestjs/entity';
 import { Test1Entity } from '@/entity/Test2Entity';
 import { getMemoryOrm } from '@/getMemoryOrm';
 
 export async function runTest1() {
+  checkMikroOrmEnv({
+    Entity,
+    MetadataStorage,
+  });
   const orm = await getMemoryOrm();
   const em = orm.em.fork();
 
   await em.findAll(Test1Entity, {
     where: {
+      code: '',
       notes: '',
     },
   });
